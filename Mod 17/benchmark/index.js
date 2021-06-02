922c6e7f3c75bbfd3784943306d25b7c095dcb32
Emily Salinas <emilyrh1058@gmail.com>
2021-06-01 23:28:34 -0500

    m

--- Mod 17/benchmark/index.js
+++ Mod 17/benchmark/index.js
@@ -1,13 +1,23 @@
 const Benchmark = require('benchmark');
+const { linearSearch, binarySearch } = require('./search');
+
+const numbers = [];
+for (let i = 1; i <= 1000000; i++) {
+  // populate the array with the numbers 1 to 1,000,000
+  numbers.push(i);
+}
+
+// grab the last number in the array as the number we want to find
+const target = numbers[numbers.length-1];
 
 const suite = new Benchmark.Suite;
 
 suite
-  .add('random test', function() {
-    // benchmark generating a random number
-    Math.floor(Math.random() * 10) + 1;
+  .add('linear search', function() {
+    linearSearch(numbers, target);
   })
   .on('complete', function() {
-    console.log(this);
+    // print the test's name and average time in milliseconds
+    console.log(`${this[0].name} averaged ${this[0].stats.mean*1000} milliseconds.`);
   })
   .run();
