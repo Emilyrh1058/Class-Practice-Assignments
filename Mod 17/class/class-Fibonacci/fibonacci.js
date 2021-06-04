70f3636e036b7a610d4fa819eaa809c7dbc1bb09
Emily Salinas <emilyrh1058@gmail.com>
2021-06-03 20:21:47 -0500

    .

--- Mod 17/class/class-Fibonacci/fibonacci.js
+++ Mod 17/class/class-Fibonacci/fibonacci.js
@@ -1,8 +1,17 @@
 // Linear Approach
-const linear = num => {
+const linear = (num) => {
   // Implement the Fibonacci algorithm using the linear approach
-  // YOUR CODE HERE
-  //
+  // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+  let sequence = [1, 1];
+  for (let i = 2; i <= num; i++) {
+    let nextNumberInSequence = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextNumberInSequence);
+  }
+
+  return sequence[num - 1]
 };
+
+// THIS IS BIG 0(n)
 
 console.log(linear(10)); // => 55
--- Mod 17/class/class-factory-function/index.js
+++ Mod 17/class/class-factory-function/index.js
@@ -1,24 +1,32 @@
 const beverage = function() {
   // Create 3 variables for the `type`, `price`, and `origin` of the beverage
   // Assign them to values of your choice
-  // YOUR CODE HERE
-  //
+  
+  const type = "coffee";
+  const price = 5;
+  const origin = "America"
+
+
 
   // Return an object literal which contains the methods `product()`, `amount()`, and `place()`
   return {
-    // `product()` will print an invitation to drink the `type` of beverage
-    //  YOUR CODE HERE
-    //
-    // `amount()` will print out the `price` of the beverage
-    // YOUR CODE HERE
-    //
-    // `place()` will print out the `origin` of the beverage
-    // YOUR CODE HERE
-    //
+  // `product()` will print an invitation to drink the `type` of beverage
+    product: function(){
+      console.log(`Do you want some ${type}?`);
+    },
+  // `amount()` will print out the `price` of the beverage
+    amount: function(){
+      console.log(`The coffee costs $${price}`);
+    },
+  // `place()` will print out the `origin` of the beverage  
+    place: function(){
+      console.log(`It is from ${origin}`);
+    }
   };
 };
 
 const drink = beverage();
 // Invoke the 3 methods
-// YOUR CODE HERE
-//
+drink.product();
+drink.amount();
+drink.place();
